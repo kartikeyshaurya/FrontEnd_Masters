8fb942dcefc200a3ba3eed106470a118b6570634
kartikeyshaurya <kartikeyshaurya@gmail.com>
2022-03-02 22:20:20 +0530

    sample

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from unittest import result
 from flask import Flask, jsonify 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/<string:n>")
 
 def hello():
     return "hello"
 
 
-@app.route("/bfhl/<string:n>")
+@app.route("/bfhl/<string:n>" ,methods= ["POST"])
 
 
 def hello2(n):
@@ -18,8 +18,8 @@ def hello2(n):
         "User ID": "john_doe_17091999",
         "Email ID": "john_doe_17091999@xyz.com" ,
         "College Roll Number":"ABCD123", 
-        "Array of Numbers": list(n),
-        "Array of Alphabet" : list(n) ,
+        "Array of Numbers": (n),
+        "Array of Alphabet" : (n) ,
         
     }
     return jsonify(result)
